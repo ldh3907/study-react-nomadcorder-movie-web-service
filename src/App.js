8675240511c661f5df22f03d1bf7d07e65bda64d
ldh3907 <ldh165163@gmail.com>
2021-05-20 11:14:59 +0900

    add: axios lib , yts movie api
    add: axios lib , yts movie api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoaing: true,
+    movies: [],
   };
-
-  add = () => {
-    this.setState({ count: 1 });
+  getMovies = async () => {
+    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
   };
-  minus = () => {
-    this.setState({ count: -1 });
-  };
+  componentDidMount() {
+    this.getMovies();
+  }
   render() {
-    return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoaing } = this.state;
+    return <div>{isLoaing ? "loading..." : "we are ready"}</div>;
   }
 }
 
