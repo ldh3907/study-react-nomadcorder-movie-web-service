e5354bee0fab9da28f45eb9a2f0981d0716ee028
ldh3907 <ldh165163@gmail.com>
2021-05-23 02:34:23 +0900

    add: css file, code
    add: css file, code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
-    isLoaing: true,
+    isLoading: true,
     movies: [],
   };
   getMovies = async () => {
@@ -15,22 +16,37 @@ class App extends React.Component {
     } = await axios.get(
       "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
     );
-    this.setState({ movies, isLoaing: false });
+    this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoaing, movies } = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoaing
-          ? "loading..."
-          : movies.map((movie) => {
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader-text">Loading..</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map((movie) => {
               console.log(movie);
-              return <Movie id year title summary poster />;
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
             })}
-      </div>
+          </div>
+        )}
+      </section>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie(id, year, title, summary, poster) {
-  return <div>{title}</div>;
+function Movie({ year, title, summary, poster }) {
+  return (
+    <div class="movie">
+      <img class="movie-img" src={poster} alt={title} title={title} />
+      <div class="movie-data">
+        <h3 class="movie-title">영화 이름 : {title}</h3>
+        <h5 class="movie-year">년도 : {year}</h5>
+        <p class="movie-summary">작품 설명 : {summary}</p>
+      </div>
+    </div>
+  );
 }
 
-Movie.prototype = {
+Movie.protoType = {
   id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
